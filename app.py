b8e90436d8dcadc23fb41c3a3c4bee75411d26b0
svyatnissan <89133366+svyatnissan@users.noreply.github.com>
2021-08-28 22:06:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,12 +18,15 @@ import json
 def get_live_matches():
     r = requests.get(f'https://api.betting-api.com/1xbet/football/live/all', headers=api_headers)
     jsn = r.text
-    while True:
-        try:
-            matches = json.loads(jsn + "}]")
-            break
-        except:
-            jsn = jsn[:-1]
+    try:
+        matches = json.loads(jsn)
+    except:
+        while True:
+            try:
+                matches = json.loads(jsn + "}]")
+                break
+            except:
+                jsn = jsn[:-1]
 #     matches = json.loads(r.content.decode())
     matches = [[match['id'], match['team1'], match['team2'], match['markets']['win1']['v'], match['markets']['winX']['v'], match['markets']['win2']['v'], match['league']['name'], match['league']['league_id'], 'Live'] for match in matches[:-1] if ('win1' in match['markets'].keys()) and ('winX' in match['markets'].keys()) and ('league' in match.keys())]
     
@@ -32,12 +35,15 @@ def get_live_matches():
 def get_pre_matches():
     r = requests.get(f'https://api.betting-api.com/1xbet/football/line/all', headers=api_headers)
     jsn = r.text
-    while True:
-        try:
-            matches = json.loads(jsn + "}]")
-            break
-        except:
-            jsn = jsn[:-1]
+    try:
+        matches = json.loads(jsn)
+    except:
+        while True:
+            try:
+                matches = json.loads(jsn + "}]")
+                break
+            except:
+                jsn = jsn[:-1]
 #     matches = json.loads(r.content.decode())
     matches = [[match['id'], match['team1'], match['team2'], match['markets']['win1']['v'], match['markets']['winX']['v'], match['markets']['win2']['v'], match['league']['name'], match['league']['league_id'], 'PreMatch'] for match in matches[:-1] if ('win1' in match['markets'].keys()) and ('winX' in match['markets'].keys()) and ('league' in match.keys())]
     
